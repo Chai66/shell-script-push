62e1487d02a0de36a1ce4aa076f8512f98a10595
Chai66 <chaitanyachalumuri06@gmail.com>
2024-03-15 21:23:54 +0530

    Added 2nd script

---
+++ 05-variables.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Please enter your username :"
+
+read -s $USERNAME  # the value entered will be attached to username, -s will be hiding username from logs
+
+echo "Please enter your password :"
+
+read -s $PASSWORD  # the value entered will be attached to password, -s will be hiding username from logs
+
+echo " username is : $USERNAME , password is : PASSWORD" # Priniting this for validation
+
+
+
