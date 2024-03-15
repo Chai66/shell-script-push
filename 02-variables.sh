2f9f9f609cc672d4e8b67521e3af7531fd59b666
Chai66 <chaitanyachalumuri06@gmail.com>
2024-03-15 19:34:33 +0530

    Added 2nd script

--- 02-variables.sh
+++ 02-variables.sh
@@ -6,4 +6,4 @@ VAR_2=VS_CODE
 
 echo "Code was edited in $VAR_2 and pushed to $VAR_1, It is working"
 echo "but when checked in super putty using $VAR_1 PULL, It is not working"
-ech0 "Appreciate if you can help,Thanks in Advance!" 
+echo "Appreciate if you can help,Thanks in Advance!" 
