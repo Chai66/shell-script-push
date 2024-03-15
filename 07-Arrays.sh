9dfe56a062c05313d9df15a81f532ce9272bc7eb
Chai66 <chaitanyachalumuri06@gmail.com>
2024-03-16 00:46:23 +0530

    Added 2nd script

--- 07-Arrays.sh
+++ 07-Arrays.sh
@@ -4,3 +4,7 @@
 Fruits=("Apple" "Orange" "Grapes")
 
 echo  "First fruit is ${Fruits[0]}"
+
+echo  "First fruit is ${Fruits[1]}"
+echo  "First fruit is ${Fruits[2]}"
+echo  "All Fruits :   ${Fruits[*]}"
