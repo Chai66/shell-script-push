7c6904bf265f9f3a6b602bed087518ded33ccd83
Chai66 <chaitanyachalumuri06@gmail.com>
2024-03-15 20:54:06 +0530

    Added 2nd script

---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+VAR_1=$1
+VAR_2=$2
+
+
+echo "Code was edited in $VAR_2 and pushed to $VAR_1, It is working"
+echo "but when checked in super putty using $VAR_1 PULL, It is not working"
+echo "Appreciate if you can help,Thanks in Advance!"
