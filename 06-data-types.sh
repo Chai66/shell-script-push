2d6a6232d5ec43e688bb15107ad012b47ef7fb66
Chai66 <chaitanyachalumuri06@gmail.com>
2024-03-16 00:12:18 +0530

    Added 2nd script

--- 06-data-types.sh
+++ 06-data-types.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-NUMBER1=$1
-NUMBER2=$2
+NUMBER1=100
+NUMBER2=200
 
 SUM=$($NUMBER1+$NUMBER2)
-SUM=`echo "$NUMBER1 + $NUMBER2" | bc`
+
 echo "Total sum is : $SUM"
-SUM='echo "$NUMBER1+$NUMBER2" 
