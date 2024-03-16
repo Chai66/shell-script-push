0c57e7a95c1edfa86750b2ee12e773f49187fd6f
Chai66 <chaitanyachalumuri06@gmail.com>
2024-03-16 18:46:50 +0530

    Added 2nd script

--- 09-install-mysql.sh
+++ 09-install-mysql.sh
@@ -2,9 +2,29 @@
 
 VAR_1=$(id -u)
 
-if [ $VAR_1 -ne 0 ]
-then 
-    echo "ERROR: Please run scrit with root access"
-else
-    echo "Installed suceessfully"
-fi
+    if [ $VAR_1 -ne 0 ]
+    then 
+        echo "ERROR: Please run scrit with root access"
+    else
+        echo "Installed suceessfully"
+    fi
+
+yum install mysql -y
+VAR_1=$(id -u)
+
+    if [ $VAR_1 -ne 0 ]
+    then 
+        echo "ERROR: Installing mysql failed"
+    else
+        echo "Installing MySQl Success"
+    fi
+
+    yum install git -y
+VAR_1=$(id -u)
+
+    if [ $VAR_1 -ne 0 ]
+    then 
+        echo "ERROR: Installing GIT failed"
+    else
+        echo "Installing GIT Success"
+    fi
