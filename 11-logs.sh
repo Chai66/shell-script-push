194ebeca996e39e01378a8d2bb0954eccb1f4f9e
Chai66 <chaitanyachalumuri06@gmail.com>
2024-03-17 01:33:08 +0530

    Added 2nd script

--- 11-logs.sh
+++ 11-logs.sh
@@ -3,24 +3,27 @@
 
 VAR_1=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
 
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
 
 VALIDATE (){
     if [ $VAR_1 -ne 0 ]
     then
-        echo "ERROR: $2 ... FAILED"
+        echo "ERROR: $2 ... $R FAILED $N"
     else
-        echo "$2 ... SUCCESS"
+        echo "$2 ... $G SUCCESS $N"
     fi
 }
 
     if [ $VAR_1 -ne 0 ]
     then 
-        echo "ERROR: Please run scrit with root access"
+        echo "$R ERROR: Please run scrit with root access $N"
         exit 1 # you can give other than zero
     else
-        echo "Installed suceessfully"
+        echo " $G Installed suceessfully $N"
     fi
 
 yum install mysql -y &>> $LOGFILE
