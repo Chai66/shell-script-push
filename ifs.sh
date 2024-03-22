537d1ccb39f93a745f1a23d2d0aa91cbdb0363f8
Chai66 <chaitanyachalumuri06@gmail.com>
2024-03-23 00:58:10 +0530

    shell push

---
+++ ifs.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+FILE="/tmp/passwd"
+
+if [ ! -f $FILE ]  # ! denotes opposite and -f denotes directory exists or not
+then 
+    echo -e "$R source directory : $FILE does not exist. $N"
+fi
+
+while IFS=":" read -r username password user_id group_id user_fullname home_die shell_path
+do
+    echo "username: $username"
+    echo "password: $password"
+    echo "user Full name: $user_fullname"
+done < $FILE
