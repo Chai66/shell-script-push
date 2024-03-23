616f408373cb91b328efe706430590247485862e
Chai66 <chaitanyachalumuri06@gmail.com>
2024-03-23 20:43:12 +0530

    shell push

---
+++ 17-disk-usage.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+DISK_USAGE=$(df -hT | grep -vE 'tmp|File')
+DISK_THRESHOLD=1
+message=""
+# Trying to achieve if disk space is above 1% we should be alerted
+
+while IFS= read line
+do
+    usage=$(echo $line | awk '{print $6F}' | cut -d % -f1) # awk print will give column fro the result, cut -d will erase the percentage from the result
+    partition=$(echo $line | awk '{print $1F}') # 1F is to extract partition of disk which includes ebs vol
+    if [ if $usage -gt 
+    then
+        message+="High disk usage on $partition: $usage\n" # += is not override next rows instaed append the lines
+    fi
+
+done <<< $DISK_USAGE
+
+echo "Message: $message"
